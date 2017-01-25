df4dd1f893f5ff3f01cde935cdde04d1c26ba1a7
Ryota Fukai <ryotaf1116@gmail.com>
2017-01-25 17:00:29 +0900

    delete admin.py

--- message/admin.py
+++ message/admin.py
@@ -1,5 +1,2 @@
 from django.contrib import admin
 from .models import Message, Member
-
-admin.site.register(Message)
-admin.site.register(Member)
--- message/models.py
+++ message/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Member(models.Model):
-    year = models.CharField(max_length=20, default=0)
+    year = models.CharField(max_length=20)
     name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
@@ -15,7 +15,6 @@ class Message(models.Model):
         Member, to_field='name', related_name='create_for')
     message = models.CharField(max_length=255)
     author = models.ForeignKey(Member, to_field='name', related_name='author')
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return (u'{0}:{1}...'.format
