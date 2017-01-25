9592fd2abb72491d0c8b281ca5b6e87a25bd59c1
Ryota Fukai <ryotaf1116@gmail.com>
2017-01-25 23:38:36 +0900

    meta db_table in models

--- message/models.py
+++ message/models.py
@@ -9,13 +9,20 @@ class Member(models.Model):
         return (u'{0}:{1}...'.format
                 (self.id, self.year, self.name))
 
+    class Meta:
+        db_table = "message_member"
+
 
 class Message(models.Model):
     create_for = models.ForeignKey(
         "Member", to_field='name', related_name='create_for')
     message = models.CharField(max_length=255)
-    author = models.ForeignKey("Member", to_field='name', related_name='author')
+    author = models.ForeignKey(
+        "Member", to_field='name', related_name='author')
 
     def __str__(self):
         return (u'{0}:{1}...'.format
                 (self.id, self.message[:10], self.create_for, self.author, ))
+
+    class Meta:
+        db_table = "message_message"
