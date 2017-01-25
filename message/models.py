ed86d8aecb4616fcb37d1a0ca46433bbd8b48392
Ryota Fukai <ryotaf1116@gmail.com>
2017-01-25 23:19:31 +0900

    add "" models

--- message/models.py
+++ message/models.py
@@ -12,9 +12,9 @@ class Member(models.Model):
 
 class Message(models.Model):
     create_for = models.ForeignKey(
-        Member, to_field='name', related_name='create_for')
+        "Member", to_field='name', related_name='create_for')
     message = models.CharField(max_length=255)
-    author = models.ForeignKey(Member, to_field='name', related_name='author')
+    author = models.ForeignKey("Member", to_field='name', related_name='author')
 
     def __str__(self):
         return (u'{0}:{1}...'.format
