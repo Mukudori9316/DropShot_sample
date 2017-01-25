b13e3cf86d8203f59bffc102210ea18699113c20
Ryota Fukai <ryotaf1116@gmail.com>
2017-01-25 14:16:05 +0900

    Initial commit

---
+++ Drop_Shot/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Drop_Shot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Drop_Shot.settings")
+
+application = get_wsgi_application()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Drop_Shot.wsgi
